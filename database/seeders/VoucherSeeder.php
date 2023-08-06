b34742fb84719636b7db6d5ff44fb70af3e71ff9
AungHmueThet <seriousglobin@gmail.com>
2023-08-06 17:28:33 +0630

    done sale_seed

--- database/seeders/VoucherSeeder.php
+++ database/seeders/VoucherSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Voucher;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class VoucherSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Voucher::factory(10)->create();
     }
 }
