d3fa44b35abcb48c899b161a5cdb8c29a2e637c5
ayemin6796 <diamalt541@gmail.com>
2023-08-06 10:02:57 +0630

    auth

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix("v1")->group(function () {
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        // Route::apiResource('contact', ContactController::class);
+
+        Route::post("logout", [ApiAuthController::class, 'logout']);
+        Route::post("logout-all", [ApiAuthController::class, 'logoutAll']);
+        // Route::get("devices", [ApiAuthController::class, 'devices']);
+
+    });
+
+
+    // Route::post("register", [ApiAuthController::class, 'register']);
+    Route::post("login", [ApiAuthController::class, 'login']);
+});
