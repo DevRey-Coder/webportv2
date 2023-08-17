418c0a557156943334fe3a16244e6ec0a47ddcf7
Lextor-lb <seriousglobin@gmail.com>
2023-08-17 21:32:39 +0630

    first

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','url','ext','user_id'];
+    // protected $fillable = ['name','url','ext','user_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
