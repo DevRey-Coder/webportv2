c5f9054083cab233448181d16c202c94c6ee9de2
Lextor-lb <seriousglobin@gmail.com>
2023-09-26 02:16:59 +0630

    d

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Voucher;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Ramsey\Collection\Collection;
 
 class OverviewController extends Controller
 {
@@ -17,8 +18,53 @@ class OverviewController extends Controller
         $totalStocks = Stock::sum('quantity');
         $totalStaff = User::where('role', '=', 'staff')->count();
 
+
+//Yearly Sales
+        $start = substr(DailySale::select('time')->find(1), 16, 4);
+        $end = Carbon::now()->format('Y');
+        $collection1 = \Illuminate\Support\Collection::make();
+
+        for ($i = $start; $i <= $end; $i++) {
+            $collection1->push($i);
+        }
+
+        $yearly = $collection1->map(function ($col) {
+            $item = DailySale::whereYear('created_at', 'like', "%$col%");
+            $totalIncome = $item->sum('dailyTotal');
+            $totalExpense = $item->sum('dailyActualTotal');
+
+
+            return collect([
+                'Yearly Sales' => $col . ' : ' . $totalIncome,
+                'Total Income' => $totalIncome,
+                'Total Expense' => $totalExpense,
+            ]);
+        });
+
+
+//Weekly Sales
+        $weeklyItem = \Illuminate\Support\Collection::make();
+
+        // Get today's date
+        $today = Carbon::now()->format('Y-m-d');
+
+        // Check if a query parameter is present
+
+        // Calculate the start and end dates for the week
+        $startDate = Carbon::now()->subDays(6)->startOfDay();
+        $endDate = Carbon::now()->endOfDay();
+
+        // Query for daily totals within the specified date range
+        $dailyTotal = DailySale::whereBetween('created_at', [$startDate, $endDate]);
+        $collectionWeek = collect([
+            'Weekly Sales' => $dailyTotal->pluck('dailyTotal', 'time'),
+            'Total Income' => $dailyTotal->sum('dailyTotal'),
+            'Total Expense' => $dailyTotal->sum('dailyActualTotal'),
+        ]);
+        $weekly = $weeklyItem->push($collectionWeek);
+
 //Monthly Sales
-        $monthlyItem = collect(["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"])
+        $monthly = collect(["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"])
             ->map(function ($col) {
                 $date = Carbon::now()->format('Y');
                 $finalValue = $date . "-" . $col;
@@ -32,10 +78,8 @@ class OverviewController extends Controller
                 ]);
             });
 
-        $totalIncome = $monthlyItem->sum('Total Income');
-        $totalExpense = $monthlyItem->sum('Total Expense');
-
-        $todaySales = Voucher::latest('id')->paginate('5')->map(function ($col) {
+//Today Overview
+        $todayOverview = Voucher::latest('id')->paginate('5')->map(function ($col) {
 
             return collect([
                 'Sale Person' => $col->user->name,
@@ -48,16 +92,36 @@ class OverviewController extends Controller
         });
 
 
+        $query = request()->input('query');
+        switch ($query) {
+            case($query == 'Yearly'):
+                $sale = $yearly;
+
+                break;
+            case($query == 'Monthly'):
+                $sale = $monthly;
+                break;
+            case($query == 'Weekly'):
+                $sale = $weekly;
+                break;
+            default:
+                $sale = 'Something went wrong.';
+        }
+
+        $totalIncome = $sale->sum('Total Income');
+        $totalExpense = $sale->sum('Total Expense');
+
         $collection = collect([
             'Total Stocks' => $totalStocks,
             'Total Staff' => $totalStaff,
-            'Monthly Sales' => $monthlyItem->pluck('Monthly Sales'),
+            'Sales' => $sale->pluck($query . " Sales"),
             'Total Profit' => $totalIncome - $totalExpense,
             'Total Income' => $totalIncome,
             'Total Expense' => $totalExpense,
-            'Today Sales Overview' => $todaySales
+            'Today Sales Overview' => $todayOverview,
 
         ]);
+
         return $collection;
     }
 }
